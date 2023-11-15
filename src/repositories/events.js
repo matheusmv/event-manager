2f72db684f609b8cf3f8f5b01e1f34532458beb3
Matheus Matias <matheus.matias.viana@hotmail.com>
2023-11-15 02:54:05 -0300

    add repository abstraction

---
+++ src/repositories/events.js
@@ -0,0 +1,92 @@
+export class EventRepository {
+    constructor(prisma) {
+        this.prisma = prisma;
+    }
+
+    async saveEvent(data) {
+        return this.prisma.event.create({
+            data: data,
+        });
+    }
+
+    async saveEventWithCategoryAndLocation(eventDetails) {
+        const {
+            name,
+            date,
+            description,
+            category,
+            local: {
+                cep,
+                state,
+                city,
+                neighborhood,
+                street,
+                number,
+                complement,
+            },
+        } = eventDetails;
+
+        return this.prisma.event.create({
+            data: {
+                name,
+                date,
+                description,
+                category: {
+                    connect: { name: category },
+                },
+                local: {
+                    create: {
+                        cep,
+                        state,
+                        city,
+                        neighborhood,
+                        street,
+                        number,
+                        complement,
+                    },
+                },
+            },
+        });
+    }
+
+    async findEventById(eventId, select = undefined) {
+        return this.prisma.event.findFirst({
+            where: {
+                id: eventId,
+            },
+            select: select,
+        });
+    }
+
+    async findEventByDate(eventDate, select = undefined) {
+        return this.prisma.event.findFirst({
+            where: {
+                date: eventDate,
+            },
+            select: select,
+        });
+    }
+
+    async findAllEvents(select = undefined) {
+        return this.prisma.event.findMany({
+            select: select,
+        });
+    }
+
+    async updateEvent(eventId, data) {
+        return this.prisma.event.update({
+            where: {
+                id: eventId,
+            },
+            data: data,
+        });
+    }
+
+    async deteleEvent(eventId) {
+        return this.prisma.event.delete({
+            where: {
+                id: eventId,
+            },
+        });
+    }
+}
---
+++ src/repositories/locations.js
@@ -0,0 +1,52 @@
+export class LocationRepository {
+    constructor(prisma) {
+        this.prisma = prisma;
+    }
+
+    async saveLocation(data) {
+        return this.prisma.local.create({
+            data: data,
+        });
+    }
+
+    async findLocationById(locationId, select = undefined) {
+        return this.prisma.local.findFirst({
+            where: {
+                id: locationId,
+            },
+            select: select,
+        });
+    }
+
+    async findLocationByCep(locationCep, select = undefined) {
+        return this.prisma.local.findFirst({
+            where: {
+                cep: locationCep,
+            },
+            select: select,
+        });
+    }
+
+    async findAllLocations(select = undefined) {
+        return this.prisma.local.findMany({
+            select: select,
+        });
+    }
+
+    async updateLocation(locationId, data) {
+        return this.prisma.local.update({
+            where: {
+                id: locationId,
+            },
+            data: data,
+        });
+    }
+
+    async deteleLocation(locationId) {
+        return this.prisma.local.delete({
+            where: {
+                id: locationId,
+            },
+        });
+    }
+}
