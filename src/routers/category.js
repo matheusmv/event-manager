a852af11fff0d22e9daf90ef8dc683bdeac1291e
Matheus Matias <matheus.matias.viana@hotmail.com>
2023-11-16 23:21:26 -0300

    add authentication & authorization on category routes - post, put, delete

--- src/routers/category.js
+++ src/routers/category.js
@@ -1,5 +1,14 @@
 import prismaClient from '../db/index.js';
 
+import { UserRepository } from '../repositories/users.js';
+
+import { AuthenticationService } from '../services/auth.js';
+
+import {
+    ensureAuthentication,
+    ensureAuthorization,
+} from '../middlewares/auth.js';
+
 import { CategoryRepository } from '../repositories/categories.js';
 
 import { CategoryService } from '../services/categories.js';
@@ -15,12 +24,30 @@ import {
 export function buildCategoryRoute(router) {
     const endPoint = '/api/v1/categories';
 
+    const userRepository = new UserRepository(prismaClient);
+    const authService = new AuthenticationService(userRepository);
+
     const categoryRepository = new CategoryRepository(prismaClient);
     const categoryService = new CategoryService(categoryRepository);
 
     router.get(endPoint, getAllCategories(categoryService));
     router.get(`${endPoint}/:id`, getCategoryById(categoryService));
-    router.post(endPoint, createCategory(categoryService));
-    router.put(`${endPoint}/:id`, updateCategory(categoryService));
-    router.delete(`${endPoint}/:id`, deleteCategory(categoryService));
+    router.post(
+        endPoint,
+        ensureAuthentication(authService),
+        ensureAuthorization(['ADMIN', 'MANAGER']),
+        createCategory(categoryService),
+    );
+    router.put(
+        `${endPoint}/:id`,
+        ensureAuthentication(authService),
+        ensureAuthorization(['ADMIN', 'MANAGER']),
+        updateCategory(categoryService),
+    );
+    router.delete(
+        `${endPoint}/:id`,
+        ensureAuthentication(authService),
+        ensureAuthorization(['ADMIN', 'MANAGER']),
+        deleteCategory(categoryService),
+    );
 }
