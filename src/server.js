8b797e28ccea80428d424dce8b673e5dcdbcb663
Matheus Matias <matheus.matias.viana@hotmail.com>
2023-11-17 22:20:15 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import cors from 'cors';
 import { getRouters } from './routers/index.js';
 import { globalErrorHandler } from './middlewares/errors.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(getRouters());
 app.use(globalErrorHandler());
